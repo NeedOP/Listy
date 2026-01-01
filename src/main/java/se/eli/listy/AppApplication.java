bdcacee388d890aa6d29c23286fc29d7565ec47d
maeka <maek.alekdawi@gmail.com>
2026-01-01 15:00:50 +0100

    Added Docker and application prop

--- src/main/java/se/eli/listy/AppApplication.java
+++ src/main/java/se/eli/listy/AppApplication.java
@@ -1,4 +1,13 @@
 package se.eli.listy;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+@SpringBootApplication
+@EnableScheduling
 public class AppApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(AppApplication.class, args);
+    }
 }
